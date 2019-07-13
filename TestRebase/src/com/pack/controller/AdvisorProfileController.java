e923d20cc12cfbd8906fde94ebc54248d1fc93e0
rasmithe1 <rasmiranjan.pradhan01@gmail.com>
2019-07-13 11:29:02 +0530

    advisor myProfile added by branch2

---
+++ TestRebase/src/com/pack/controller/AdvisorProfileController.java
@@ -0,0 +1,8 @@
+package com.pack.controller;
+
+public class AdvisorProfileController {
+
+	public static void main(String[] args) {
+		System.out.println("Advisor myProfile added by mybranch2");
+	}
+}
