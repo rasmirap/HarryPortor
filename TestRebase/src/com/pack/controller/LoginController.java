09c92f3e2bb949cbd977ecc697eb522a6c9279b3
rasmithe1 <rasmiranjan.pradhan01@gmail.com>
2019-07-13 09:40:20 +0530

    myBranch1 first commit

--- TestRebase/src/com/pack/controller/LoginController.java
+++ TestRebase/src/com/pack/controller/LoginController.java
@@ -2,6 +2,6 @@ package com.pack.controller;
 
 public class LoginController {
 	public static void main(String[] args) {
-		System.out.println("Master first commit.");
+		System.out.println("MyBranch1 first commit.");
 	}
 }
