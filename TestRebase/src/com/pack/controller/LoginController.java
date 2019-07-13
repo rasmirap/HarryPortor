8addf026a32ea6a94b5f31f66e357451069b1823
rasmithe1 <rasmiranjan.pradhan01@gmail.com>
2019-07-13 10:01:30 +0530

    Master second commit

--- TestRebase/src/com/pack/controller/LoginController.java
+++ TestRebase/src/com/pack/controller/LoginController.java
@@ -4,4 +4,9 @@ public class LoginController {
 	public static void main(String[] args) {
 		System.out.println("MyBranch1 first commit.");
 	}
+	
+	public void add() {
+		System.out.println("Master second commit.");
+		
+	}
 }
